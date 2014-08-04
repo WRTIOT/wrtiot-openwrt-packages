bb59f1eeb2d786f191e748d5492ae28045b2bb07
qinfengling <Fengling.Qin@gmail.com>
2014-08-05 01:01:56 +0800

    Support control through tcp long polling

--- luasrc/IoTd.lua
+++ luasrc/IoTd.lua
@@ -27,6 +27,15 @@
 
 local actREST = require ("wrtiot.actREST")
 local uci = require("luci.model.uci")
+local Json = require("luci.json")
+local host, port = "tcp.lewei50.com", 9960
+local socket = require("socket")
+
+uci = uci.cursor()
+
+local IOT = uci:get_all("wrtiot","IOT")
+
+local API_KEY = IOT.API_KEY
 
 function process_data_REST(d)		-- Process the data use RESTful protocol
         data = {}
@@ -39,13 +48,62 @@ end
 serialin=io.open("/dev/ttyUSB0","rb")   --open serial port and prepare to read data from dongle
 
 while true do
-	while raw_data == nill do 
-		serialin:flush()
-		raw_data = serialin:read()
+	--read command from lewei,keep alive through tcp
+	if commandserver == nil then
+		commandserver = socket.tcp()
+		commandserver:settimeout(3)
+		commandserver:connect(host, port)
+
+		os.execute("logger TCP Connect")
+		msg = {
+			["method"] = "update",
+			["gatewayNo"] = "01",
+			["userkey"] = API_KEY
+		}
+
+		msg = Json.encode(msg)
+		msg = msg .. "&^!"
+
+		commandserver:send(msg)
 	end
+
+	lasttime = os.time()
+	local s, status, partial = commandserver:receive('*a')
+	if partial ~= "" then
+		os.execute("logger recv command is: " .. string.match(partial, "(.+)&^!"))
+		command = Json.decode(string.match(partial,"(.+)&^!"))
+		os.execute("logger command fun is: " .. command['f'])
+		resp = {
+			["method"] = "response",
+			["result"] = {["successful"] = true, ["message"] = "WRTIOT Resp!"}
+		}
+		resp = Json.encode(resp)
+		resp = resp .. "&^!"
+		commandserver:send(resp)
+		os.execute("logger send resp is: " .. resp)
+		partial = nil
+	end
+
+	if status == "closed" then
+		commandserver:close()
+		commandserver = nil
+		os.execute("logger TCP Closed")
+	end
+	if os.time() - lasttime > 30 then
+		lasttime = os.time()
+		commandserver:send(msg)
+		os.execute("logger send msg is: " .. msg)
+	end
+
+	--read data from dongle
+	serialin:flush()
+	raw_data = serialin:read()
+	if raw_data ~= nill then
 	os.execute("logger raw data is: " .. raw_data)
 	process_data_REST(raw_data)
 	raw_data = nill
+	end
+
 end
 serialin:close()
 
